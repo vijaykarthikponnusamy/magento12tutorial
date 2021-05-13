6076d6d622e872465ff7cea3e01ce40a27355b73
Vijaykarthik Ponnusamy <vijaykarthik.ponnusamy@ziffity.com>
2021-05-13 20:46:28 +0530

    Task Register Custom Module

---
+++ app/code/MyCompany/TaskEmpty/registration.php
@@ -0,0 +1,7 @@
+  <?php
+  \Magento\Framework\Component\ComponentRegistrar::register(
+      \Magento\Framework\Component\ComponentRegistrar::MODULE,
+      'MyCompany_TaskEmpty',
+      __DIR__
+  );
+
